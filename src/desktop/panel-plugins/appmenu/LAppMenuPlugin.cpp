c9559848ce875ec96a02da7ec19097d69424e3af
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2019-05-06 09:33:47 +0200

    minor

--- src/desktop/panel-plugins/appmenu/LAppMenuPlugin.cpp
+++ src/desktop/panel-plugins/appmenu/LAppMenuPlugin.cpp
@@ -82,19 +82,18 @@ void LAppMenuPlugin::UpdateMenu(){
   QHash<QString, QList<XDGDesktop*> > *HASH = LSession::handle()->applicationMenu()->currentAppHash();
     //Now Re-create the menu (orignally copied from the AppMenu class)
 
-    // Add link to web browser
-    QString browserPath = LXDG::findDefaultAppForMime("x-scheme-handler/http");
-    XDGDesktop browser(browserPath);
-    if (browser.isValid()) {
-       QAction *browserAction = mainmenu->addAction(LXDG::findIcon("web-browser", ""), tr("Web Browser"));
-       browserAction->setWhatsThis(browserPath);
-    }
-
     // Add link to the file manager
     QAction *tmpact = mainmenu->addAction( LXDG::findIcon("user-home", ""), tr("Browse Files") );
     tmpact->setWhatsThis("\""+QDir::homePath()+"\"");
     tmpact = mainmenu->addAction( LXDG::findIcon("user-home", ""), tr("Browse Files") );
 
+    // Add link to web browser
+    QString browserPath = LXDG::findDefaultAppForMime("x-scheme-handler/http");
+    XDGDesktop browser(browserPath);
+    if (browser.isValid()) {
+       tmpact = mainmenu->addAction(LXDG::findIcon("web-browser", ""), tr("Web Browser"));
+       tmpact->setWhatsThis(browserPath);
+    }
     //--Look for the app store
     /*XDGDesktop store(LOS::AppStoreShortcut());
     if(store.isValid()){
