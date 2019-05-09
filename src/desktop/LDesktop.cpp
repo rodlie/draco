244e88becb273146fbb81461e133cbfe14d36257
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2019-05-09 19:24:39 +0200

    minor

--- src/desktop/LDesktop.cpp
+++ src/desktop/LDesktop.cpp
@@ -384,7 +384,7 @@ void LDesktop::UpdateMenu(bool fast)
     // Put a label at the top
     int num = LSession::handle()->XCB->CurrentWorkspace(); //LX11::GetCurrentDesktop();
     qDebug() << "Found workspace number:" << num;
-    workspacelabel->setText(QString("%1 %2").arg(DESKTOP_APP_NAME).arg(QString("%1%2").arg(DESKTOP_APP_VERSION).arg(DESKTOP_APP_VERSION_EXTRA)));
+    workspacelabel->setText(QString("%1 Workspace %2").arg(DESKTOP_APP_NAME).arg(num));
 
     if (fast && usewinmenu) { UpdateWinMenu(); }
     if (fast) { return; } // already done
