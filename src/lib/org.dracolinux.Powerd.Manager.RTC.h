585e206f49e460b412962b580b6b74d25762ac81
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2019-05-13 05:04:45 +0200

    fix build and lic

--- src/lib/org.dracolinux.Powerd.Manager.RTC.h
+++ src/lib/org.dracolinux.Powerd.Manager.RTC.h
@@ -1,11 +1,12 @@
 /*
 #
 # Draco Desktop Environment <https://dracolinux.org>
-# Copyright (c) 2019, Ole-André Rodlie <ole.andre.rodlie@gmail.com> All rights reserved.
+# Copyright (c) 2019, Ole-André Rodlie <ole.andre.rodlie@gmail.com>
+# All rights reserved.
 #
 * This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
+* it under the terms of the GNU Lesser Public License as published by
+* the Free Software Foundation; either version 2.1 of the License, or
 * (at your option) any later version.
 *
 * This program is distributed in the hope that it will be useful,
@@ -13,7 +14,7 @@
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
 *
-* You should have received a copy of the GNU General Public License
+* You should have received a copy of the GNU Lesser Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>
 #
 */
